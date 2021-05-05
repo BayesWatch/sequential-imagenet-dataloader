f76899c691e36af7b87d203e5f587a1e143f2cc8
Gavin <gavingray1729@gmail.com>
2021-05-05 17:00:17 -0400

    users shouldn't have to set env vars

--- preprocess_sequential.py
+++ preprocess_sequential.py
@@ -2,10 +2,15 @@
 
 import os
 import numpy as np
-from tensorpack.dataflow import *
-os.environ['TENSORPACK_DATASET'] = '/home/shgao/tensorpack_data/'
-os.environ['IMAGENET'] = '/media/db/datasets/ILSVRC/Data/CLS-LOC/'
+from tensorpack.dataflow import dataset, PrefetchDataZMQ, LMDBSerializer
+import argparse
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Preprocess ImageNet into an LMDB file.')
+    parser.add_argument('imagenet', type=str, help='location of ImageNet directory')
+    parser.add_argument('database_dir', type=str, default=None, help='location to save output database')
+    args = parser.parse_args()
+
     class BinaryILSVRC12(dataset.ILSVRC12Files):
         def get_data(self):
             for fname, label in super(BinaryILSVRC12, self).__iter__():
@@ -13,9 +18,14 @@ if __name__ == '__main__':
                     jpeg = f.read()
                 jpeg = np.asarray(bytearray(jpeg), dtype='uint8')
                 yield [jpeg, label]
-    imagenet_path = os.environ['IMAGENET']
+    if args.database_dir is None:
+        lmdb_path = args.imagenet
+    else:
+        lmdb_path = args.database_dir 
+    os.environ['TENSORPACK_DATASET'] = os.path.join(lmdb_path, "tensorpack_data")
 
     for name in ['train', 'val']:
-        ds0 = BinaryILSVRC12(imagenet_path, name)
+        print(f"Processing {args.imagenet} {name}...")
+        ds0 = BinaryILSVRC12(args.imagenet, name)
         ds1 = PrefetchDataZMQ(ds0, nr_proc=1)
-        LMDBSerializer.save(ds1, os.path.join(imagenet_path,'ILSVRC-%s.lmdb'%name))
+        LMDBSerializer.save(ds1, os.path.join(lmdb_path, 'ILSVRC-%s.lmdb'%name))

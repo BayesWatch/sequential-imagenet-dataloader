532f3bd667da1ca3f0b986b3de754b23ca92ee95
Gavin <gavingray1729@gmail.com>
2021-05-07 19:19:42 -0400

    fixed single threaded 1.5s/it

--- imagenet_seq/data.py
+++ imagenet_seq/data.py
@@ -7,52 +7,9 @@ import cv2
 import torch
 
 import numpy as np
-import tensorpack.dataflow as td
-from tensorpack import imgaug
-from tensorpack.utils.serialize import loads
-
-##############################################################################################################
-# copied from: https://github.com/tensorpack/tensorpack/blob/master/examples/ImageNetModels/imagenet_utils.py#
-############################################################################################################## 
-def fbresnet_augmentor(isTrain):
-    """
-    Augmentor used in fb.resnet.torch, for BGR images in range [0,255].
-    """
-    interpolation = cv2.INTER_CUBIC
-    # linear seems to have more stable performance.
-    # but we keep cubic for compatibility with old models
-    if isTrain:
-        augmentors = [
-            imgaug.GoogleNetRandomCropAndResize(interp=interpolation),
-            imgaug.ToFloat32(),  # avoid frequent casting in each color augmentation
-            # It's OK to remove the following augs if your CPU is not fast enough.
-            # Removing brightness/contrast/saturation does not have a significant effect on accuracy.
-            # Removing lighting leads to a tiny drop in accuracy.
-            imgaug.RandomOrderAug(
-                [imgaug.BrightnessScale((0.6, 1.4)),
-                 imgaug.Contrast((0.6, 1.4), rgb=False),
-                 imgaug.Saturation(0.4, rgb=False),
-                 # rgb-bgr conversion for the constants copied from fb.resnet.torch
-                 imgaug.Lighting(0.1,
-                                 eigval=np.asarray(
-                                     [0.2175, 0.0188, 0.0045][::-1]) * 255.0,
-                                 eigvec=np.array(
-                                     [[-0.5675, 0.7192, 0.4009],
-                                      [-0.5808, -0.0045, -0.8140],
-                                      [-0.5836, -0.6948, 0.4203]],
-                                     dtype='float32')[::-1, ::-1]
-                                 )]),
-            imgaug.ToUint8(),
-            imgaug.Flip(horiz=True),
-        ]
-    else:
-        augmentors = [
-            imgaug.ResizeShortestEdge(256, interp=interpolation),
-            imgaug.CenterCrop((224, 224)),
-        ]
-    return augmentors
-#####################################################################################################
-#####################################################################################################
+import dataflow as td
+from io import BytesIO
+from PIL import Image
 
 
 class ImagenetLoader(object):
@@ -79,35 +36,37 @@ class ImagenetLoader(object):
             GPU, which is faster).
     """
 
-    def __init__(self, imagenet_dir, mode, batch_size=256, shuffle=False, num_workers=4, cache=50000,
+    def __init__(self, imagenet_dir, mode, transform, batch_size=256, shuffle=False, num_workers=4, cache=50000,
             drop_last=False, cuda=False):
         if drop_last:
             raise NotImplementedError("drop_last not implemented")
         # enumerate standard imagenet augmentors
         assert mode in ['train', 'val'], mode
-        augmentors = fbresnet_augmentor(mode == 'train')
 
         # load the lmdb if we can find it
         lmdb_loc = os.path.join(imagenet_dir, 'ILSVRC-%s.lmdb'%mode)
-        ds = td.LMDBData(lmdb_loc, shuffle=False)
-        ds = td.MapData(ds, td.LMDBSerializer._deserialize_lmdb) 
-        ds = td.MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
-        # ds = td.LMDBSerializer.load(lmdb_loc, shuffle=False)
-        # ds = td.LocallyShuffleData(ds, cache)
+        # ds = td.LMDBData(lmdb_loc, shuffle=False)
+        # ds = td.MapData(ds, td.LMDBSerializer._deserialize_lmdb) 
+        # ds = td.MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
+        ds = td.LMDBSerializer.load(lmdb_loc, shuffle=False)
+        ds = td.LocallyShuffleData(ds, cache)
         # ds = td.PrefetchData(ds, batch_size*num_workers, 1)
         # ds = td.MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
         # ds = td.MapDataComponent(ds, lambda x: loads(x), 0)
         # ds = td.AugmentImageComponent(ds, imagenet_augmentors)
         # https://github.com/tensorpack/tensorpack/issues/1405
-        # def f(x):
-        #     img, label = td.LMDBSerializer._deserialize_lmdb(x)
-        #     return imagenet_augmentors.augment(cv2.imdecode(img, cv2.IMREAD_COLOR)), label
+        def f(x):
+            img, label = x
+            img = Image.open(BytesIO(img.tobytes())).convert('RGB')
+            img = transform(img)
+            return img, label
         # def g(x):
         #     return imagenet_augmentors.augment(cv2.imdecode(x, cv2.IMREAD_COLOR))
-        ds = td.AugmentImageComponent(ds, augmentors, copy=False)
-        ds = td.MultiProcessRunnerZMQ(ds, num_proc=num_workers)
+        # ds = td.AugmentImageComponent(ds, augmentors, copy=False)
+        # ds = td.MultiProcessRunnerZMQ(ds, num_proc=num_workers)
         # ds = td.MultiProcessMapDataZMQ(ds, num_proc=num_workers, map_func=f)
-        self.ds = td.BatchData(ds, batch_size, remainder=False)
+        ds = td.MapData(ds, f)
+        self.ds = td.BatchData(ds, batch_size, use_list=True, remainder=False)
         self.ds.reset_state()
 
         self.batch_size = batch_size
@@ -116,14 +75,8 @@ class ImagenetLoader(object):
 
     def __iter__(self):
         for x, y in self.ds.get_data():
-            if self.cuda:
-                # images come out as uint8, which are faster to copy onto the gpu
-                x = torch.ByteTensor(x).cuda()
-                y = torch.IntTensor(y).cuda()
-                # but once they're on the gpu, we'll need them in float
-                yield uint8_to_float(x), y.long()
-            else:
-                yield uint8_to_float(torch.ByteTensor(x)), torch.IntTensor(y).long()
+            x, y = torch.stack(x), torch.tensor(y)
+            yield x, y
 
     def __len__(self):
         return self.ds.size()
@@ -134,6 +87,17 @@ def uint8_to_float(x):
 
 if __name__ == '__main__':
     from tqdm import tqdm
-    dl = ImagenetLoader(os.environ['DBLOC'], 'train', cuda=True, num_workers=4)
-    for x in tqdm(dl, total=len(dl)):
+    import torchvision.transforms as transforms
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    dl = ImagenetLoader(os.environ['DBLOC'], 'train', transform=transform, cuda=True, num_workers=4)
+    for x, y in tqdm(dl, total=len(dl)):
+        x, y = x.cuda(), y.cuda()
         pass

aaffef255e2306212993e1fc0e208a2be9852c42
Gavin <gavingray1729@gmail.com>
2021-05-07 18:39:57 -0400

    speed testing

--- imagenet_seq/data.py
+++ imagenet_seq/data.py
@@ -9,58 +9,28 @@ import torch
 import numpy as np
 import tensorpack.dataflow as td
 from tensorpack import imgaug
+from tensorpack.utils.serialize import loads
 
-#####################################################################################################
-# copied from: https://github.com/ppwwyyxx/tensorpack/blob/master/examples/ResNet/imagenet_utils.py #
-#####################################################################################################
-class GoogleNetResize(imgaug.ImageAugmentor):
-    """
-    crop 8%~100% of the original image
-    See `Going Deeper with Convolutions` by Google.
-    """
-    def __init__(self, crop_area_fraction=0.08,
-                 aspect_ratio_low=0.75, aspect_ratio_high=1.333,
-                 target_shape=224):
-        self._init(locals())
-
-    def get_transform(self, img):
-        def legacy_augment_coords(self, coords, p):
-            return coords 
-        return imgaug.TransformFactory(name="LegacyConversion --"+str(self),
-                    apply_image=lambda img: self._augment(img),
-                    apply_coords=lambda coords: legacy_augment_coords(self, coords, p))
-
-    def _augment(self, img):
-        h, w = img.shape[:2]
-        area = h * w
-        for _ in range(10):
-            targetArea = self.rng.uniform(self.crop_area_fraction, 1.0) * area
-            aspectR = self.rng.uniform(self.aspect_ratio_low, self.aspect_ratio_high)
-            ww = int(np.sqrt(targetArea * aspectR) + 0.5)
-            hh = int(np.sqrt(targetArea / aspectR) + 0.5)
-            if self.rng.uniform() < 0.5:
-                ww, hh = hh, ww
-            if hh <= h and ww <= w:
-                x1 = 0 if w == ww else self.rng.randint(0, w - ww)
-                y1 = 0 if h == hh else self.rng.randint(0, h - hh)
-                out = img[y1:y1 + hh, x1:x1 + ww]
-                out = cv2.resize(out, (self.target_shape, self.target_shape), interpolation=cv2.INTER_CUBIC)
-                return out
-        out = imgaug.ResizeShortestEdge(self.target_shape, interp=cv2.INTER_CUBIC).augment(img)
-        out = imgaug.CenterCrop(self.target_shape).augment(out)
-        return out
-
-
+##############################################################################################################
+# copied from: https://github.com/tensorpack/tensorpack/blob/master/examples/ImageNetModels/imagenet_utils.py#
+############################################################################################################## 
 def fbresnet_augmentor(isTrain):
     """
     Augmentor used in fb.resnet.torch, for BGR images in range [0,255].
     """
+    interpolation = cv2.INTER_CUBIC
+    # linear seems to have more stable performance.
+    # but we keep cubic for compatibility with old models
     if isTrain:
         augmentors = [
-            GoogleNetResize(),
+            imgaug.GoogleNetRandomCropAndResize(interp=interpolation),
+            imgaug.ToFloat32(),  # avoid frequent casting in each color augmentation
+            # It's OK to remove the following augs if your CPU is not fast enough.
+            # Removing brightness/contrast/saturation does not have a significant effect on accuracy.
+            # Removing lighting leads to a tiny drop in accuracy.
             imgaug.RandomOrderAug(
-                [imgaug.BrightnessScale((0.6, 1.4), clip=False),
-                 imgaug.Contrast((0.6, 1.4), clip=False),
+                [imgaug.BrightnessScale((0.6, 1.4)),
+                 imgaug.Contrast((0.6, 1.4), rgb=False),
                  imgaug.Saturation(0.4, rgb=False),
                  # rgb-bgr conversion for the constants copied from fb.resnet.torch
                  imgaug.Lighting(0.1,
@@ -72,69 +42,17 @@ def fbresnet_augmentor(isTrain):
                                       [-0.5836, -0.6948, 0.4203]],
                                      dtype='float32')[::-1, ::-1]
                                  )]),
+            imgaug.ToUint8(),
             imgaug.Flip(horiz=True),
         ]
     else:
         augmentors = [
-            imgaug.ResizeShortestEdge(256, cv2.INTER_CUBIC),
+            imgaug.ResizeShortestEdge(256, interp=interpolation),
             imgaug.CenterCrop((224, 224)),
         ]
-    return imgaug.AugmentorList(augmentors)
+    return augmentors
 #####################################################################################################
 #####################################################################################################
-
-
-numpy_type_map = {
-    'float64': torch.DoubleTensor,
-    'float32': torch.FloatTensor,
-    'float16': torch.HalfTensor,
-    'int64': torch.LongTensor,
-    'int32': torch.IntTensor,
-    'int16': torch.ShortTensor,
-    'int8': torch.CharTensor,
-    'uint8': torch.ByteTensor,
-}
-
-
-def default_collate(batch):
-    "Puts each data field into a tensor with outer dimension batch size"
-
-    error_msg = "batch must contain tensors, numbers, dicts or lists; found {}"
-    elem_type = type(batch[0])
-    if torch.is_tensor(batch[0]):
-        out = None
-        if _use_shared_memory:
-            # If we're in a background process, concatenate directly into a
-            # shared memory tensor to avoid an extra copy
-            numel = sum([x.numel() for x in batch])
-            storage = batch[0].storage()._new_shared(numel)
-            out = batch[0].new(storage)
-        return torch.stack(batch, 0, out=out)
-    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        elem = batch[0]
-        if elem_type.__name__ == 'ndarray':
-            # array of string classes and object
-            if re.search('[SaUO]', elem.dtype.str) is not None:
-                raise TypeError(error_msg.format(elem.dtype))
-
-            return torch.stack([torch.from_numpy(b) for b in batch], 0)
-        if elem.shape == ():  # scalars
-            py_type = float if elem.dtype.name.startswith('float') else int
-            return numpy_type_map[elem.dtype.name](list(map(py_type, batch)))
-    elif isinstance(batch[0], int):
-        return torch.LongTensor(batch)
-    elif isinstance(batch[0], float):
-        return torch.DoubleTensor(batch)
-    elif isinstance(batch[0], string_classes):
-        return batch
-    elif isinstance(batch[0], collections.Mapping):
-        return {key: default_collate([d[key] for d in batch]) for key in batch[0]}
-    elif isinstance(batch[0], collections.Sequence):
-        transposed = zip(*batch)
-        return [default_collate(samples) for samples in transposed]
-
-    raise TypeError((error_msg.format(type(batch[0]))))
 
 
 class ImagenetLoader(object):
@@ -145,12 +63,12 @@ class ImagenetLoader(object):
     Arguments:
         mode (str, required): mode of dataset to operate in, one of ['train', 'val']
         batch_size (int, optional): how many samples per batch to load
-            (default: 1).
+            (default: 256).
         shuffle (bool, optional): set to ``True`` to have the data reshuffled
             at every epoch (default: False).
         num_workers (int, optional): how many subprocesses to use for data
             loading. 0 means that the data will be loaded in the main process
-            (default: 0)
+            (default: 4)
         cache (int, optional): cache size to use when loading data,
         drop_last (bool, optional): set to ``True`` to drop the last incomplete batch,
             if the dataset size is not divisible by the batch size. If ``False`` and
@@ -161,26 +79,35 @@ class ImagenetLoader(object):
             GPU, which is faster).
     """
 
-    def __init__(self, imagenet_dir, mode, batch_size=256, shuffle=False, num_workers=25, cache=50000,
-            collate_fn=default_collate,  drop_last=False, cuda=False):
+    def __init__(self, imagenet_dir, mode, batch_size=256, shuffle=False, num_workers=4, cache=50000,
+            drop_last=False, cuda=False):
         if drop_last:
             raise NotImplementedError("drop_last not implemented")
         # enumerate standard imagenet augmentors
-        imagenet_augmentors = fbresnet_augmentor(mode == 'train')
+        assert mode in ['train', 'val'], mode
+        augmentors = fbresnet_augmentor(mode == 'train')
 
         # load the lmdb if we can find it
         lmdb_loc = os.path.join(imagenet_dir, 'ILSVRC-%s.lmdb'%mode)
-        ds = td.LMDBSerializer.load(lmdb_loc, shuffle=False)
-        ds = td.LocallyShuffleData(ds, cache)
-        # ds = td.PrefetchData(ds, 5000, 1)
+        ds = td.LMDBData(lmdb_loc, shuffle=False)
+        ds = td.MapData(ds, td.LMDBSerializer._deserialize_lmdb) 
+        ds = td.MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
+        # ds = td.LMDBSerializer.load(lmdb_loc, shuffle=False)
+        # ds = td.LocallyShuffleData(ds, cache)
+        # ds = td.PrefetchData(ds, batch_size*num_workers, 1)
         # ds = td.MapDataComponent(ds, lambda x: cv2.imdecode(x, cv2.IMREAD_COLOR), 0)
+        # ds = td.MapDataComponent(ds, lambda x: loads(x), 0)
         # ds = td.AugmentImageComponent(ds, imagenet_augmentors)
         # https://github.com/tensorpack/tensorpack/issues/1405
-        def f(x):
-            jpeg_str, label = x
-            return imagenet_augmentors.augment(cv2.imdecode(jpeg_str, cv2.IMREAD_COLOR)), label
-        ds = td.MultiProcessMapDataZMQ(ds, num_proc=num_workers, map_func=f)
-        self.ds = td.BatchData(ds, batch_size)
+        # def f(x):
+        #     img, label = td.LMDBSerializer._deserialize_lmdb(x)
+        #     return imagenet_augmentors.augment(cv2.imdecode(img, cv2.IMREAD_COLOR)), label
+        # def g(x):
+        #     return imagenet_augmentors.augment(cv2.imdecode(x, cv2.IMREAD_COLOR))
+        ds = td.AugmentImageComponent(ds, augmentors, copy=False)
+        ds = td.MultiProcessRunnerZMQ(ds, num_proc=num_workers)
+        # ds = td.MultiProcessMapDataZMQ(ds, num_proc=num_workers, map_func=f)
+        self.ds = td.BatchData(ds, batch_size, remainder=False)
         self.ds.reset_state()
 
         self.batch_size = batch_size
@@ -207,6 +134,6 @@ def uint8_to_float(x):
 
 if __name__ == '__main__':
     from tqdm import tqdm
-    dl = ImagenetLoader(os.environ['DBLOC'], 'train', cuda=True)
+    dl = ImagenetLoader(os.environ['DBLOC'], 'train', cuda=True, num_workers=4)
     for x in tqdm(dl, total=len(dl)):
         pass
